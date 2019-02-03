79cfe7f840f6898e597e4820c5f368bfc896b526
huang <huang_guibo@outlook.com>
2019-02-03 23:28:36 +0800

    优化

--- app/src/main/java/com/example/hymn/Main3Activity.java
+++ app/src/main/java/com/example/hymn/Main3Activity.java
@@ -41,8 +41,8 @@ public class Main3Activity extends AppCompatActivity {
                 .addLink(R.mipmap.wb,"weibo","https://weibo.com/u/5242479890")
                 .addLink(R.mipmap.sharp_feedback_black,"Feedback","huang_guibo@outlook.com")
                 .addAction(R.drawable.web,"生命之光网站","http://www.smzg.com/")
-                .addAction(R.drawable.dwonload,"更新","https://github.com/IsGuibo/MyApplication/blob/master/app/release/app-release.apk?raw=true")
-                .addShareAction("","https://github.com/IsGuibo/MyApplication/blob/master/app/release/app-release.apk")
+                .addAction(R.drawable.dwonload,"更新","https://github.com/IsGuibo/Hymn/blob/master/app/release/app-release.apk?raw=true")
+                .addShareAction("","https://github.com/IsGuibo/Hymn/blob/master/app/release/app-release.apk")
                 .build();
         RelativeLayout.LayoutParams relLayoutParams=new RelativeLayout.LayoutParams
                 (RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT);
